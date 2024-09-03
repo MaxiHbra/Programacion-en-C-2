14e31b5e940c7ecd62f0cf04b4dd008e9c3fb701
Maxi Huebra <104871583+MaxiHbra@users.noreply.github.com>
2024-09-03 19:27:39 -0300

    Producto tradicional de A por B
    Ejercicio 6: Dados dos números naturales A y B, se desea calcular el producto de A * B utilizando la definición del producto: A*B= A+A+A...B veces.

---
+++ ej6.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+void multiplicar(int a, int b); 
+int main() { 
+	int a,b; 
+	printf("Ingresar A: "); 
+	scanf("%d",&a); 
+	printf("Ingresar B: "); 
+	scanf("%d",&b); 
+	multiplicar(a,b); 
+	return 0;
+}
+void multiplicar (int a, int b){ 
+	int i,sum; 
+	sum=0; 
+	for(i=1;i<=b;i++) { 
+		sum=sum+a;
+		printf("\nA * %d = %d",i,sum);
+	}
+}
