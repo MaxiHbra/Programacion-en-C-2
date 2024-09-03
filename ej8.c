09a782f668d3637d44efe267c1d879c0c6e3ce14
Maxi Huebra <104871583+MaxiHbra@users.noreply.github.com>
2024-09-03 19:17:22 -0300

    Add files via upload
    Ejercicio 8: Se necesita un programa en C, que simule el tiempo que esperan dos clientes, A y B, para realizar un trámite en el banco. El programa deberá mostrar cuál es el cliente que será atendido primero, considerando el menor tiempo de espera. (El tiempo de espera de cada cliente se asigna de forma aleatoria, siendo un número natural en el rango [1,30]).
    Nota: Si el rango es ingresado por el usuario, ¿cómo se modificaría la solución original?

---
+++ ej8.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+int generar_tiempo(void); 
+int main() { 
+	int a,b;
+	srand(time(NULL));
+	printf("Bienvenido al programa");
+	a=generar_tiempo();
+	b=generar_tiempo(); 
+	printf("\nEl tiempo de espera del cliente A es: %d \t El tiempo de espera del cliente B es: %d\n",a,b);
+	if (a<b)printf("\nEl cliente A será atendido primero."); 
+		else printf("\nEl cliente B será atendido primero"); 
+	return 0; 
+	}
+
+int generar_tiempo (void){ 
+	int x; 
+	x=rand()%30+1; 
+	return x;
+}
